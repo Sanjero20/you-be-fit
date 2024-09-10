8cfe4b615ee6e082de6676de3ca2726f6e0438ab
Sanjero20 <gelosantos72@gmail.com>
2024-09-10 23:07:28 +0800

    chore: setup react-router

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,45 @@
+function HomePage() {
+  return (
+    <div className="min-h-screen w-full">
+      <main className="container mx-auto h-full w-full px-4 sm:px-0">
+        {/*  */}
+        <section className="prose mx-auto py-8">
+          <h1 className="text-center">You Be Fit - Assessment Tool</h1>
+
+          <img className="rounded-xl" src="weightlift.jpg" alt="weigthlift" />
+
+          <h2>Introduction</h2>
+          <p>
+            Welcome to You Be Fit! Our tool helps you assess your overall
+            fitness with quick and easy tests. Use tools like the BMI
+            Calculator, Strength Test, Flexibility Test, and Endurance Test to
+            start improving your health today
+          </p>
+
+          <h2>Research Description</h2>
+          <p>
+            You Be Fit provides four core tools: the BMI Calculator, Strength
+            Test, Flexibility Test, and Endurance Test. These assessments give
+            users a well-rounded view of their fitness, offering easy ways to
+            track and improve their performance
+          </p>
+
+          <h2>Significance of this Research</h2>
+          <p>
+            Staying fit can be tough with a busy life. You Be Fit simplifies
+            fitness tracking with quick assessments for BMI, strength,
+            flexibility, and endurance, making it easy for anyone to stay on top
+            of their health.
+          </p>
+        </section>
+
+        <section>
+          {/*  */}
+          {/*  */}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
