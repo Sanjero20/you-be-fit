cac832aa6664bf973acad9723f84cd9b3ea41874
Sanjero20 <gelosantos72@gmail.com>
2024-09-10 22:02:40 +0800

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full">
+      <main className="container mx-auto h-full w-full">
+        <div className="prose">
+          <h1>You be fit</h1>
+          <p>Assessment tool bla bla bla</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
