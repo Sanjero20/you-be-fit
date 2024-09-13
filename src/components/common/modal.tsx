0ae399dd7399cc9247cff580f5259cbdc9776906
Sanjero20 <gelosantos72@gmail.com>
2024-09-13 23:00:51 +0800

    chore: hide modal title and description

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -1,24 +1,22 @@
-/** @format */
-
-import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog';
-import { DialogDescription } from '@radix-ui/react-dialog';
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import { DialogDescription } from "@radix-ui/react-dialog";
 
 interface ModalProps {
-	isOpen: boolean;
-	closeModal: () => void;
-	children: React.ReactNode;
+  isOpen: boolean;
+  closeModal: () => void;
+  children: React.ReactNode;
 }
 
 const Modal = ({ children, isOpen, closeModal }: ModalProps) => {
-	return (
-		<Dialog open={isOpen} onOpenChange={closeModal}>
-			<DialogContent>
-				<DialogTitle></DialogTitle>
-				<DialogDescription></DialogDescription>
-				{children}
-			</DialogContent>
-		</Dialog>
-	);
+  return (
+    <Dialog open={isOpen} onOpenChange={closeModal}>
+      <DialogContent>
+        <DialogTitle className="hidden" />
+        <DialogDescription className="hidden" />
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default Modal;
